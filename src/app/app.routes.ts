f8b65f4c9277181073f5f2272183bc1a8173fbe9
RAYAN LOPES AMERICO <rayan.americo@alunos.sc.senac.br>
2025-08-06 11:29:28 -0300

    adicionado o acesso e mais o basico

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,7 +7,20 @@ export const routes: Routes = [
   },
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'login',
     pathMatch: 'full',
   },
+  {
+    path: 'login',
+    loadComponent: () => import('./sistema-de-login/login/login.page').then( m => m.LoginPage)
+  },
+  {
+    path: 'cadastro',
+    loadComponent: () => import('./sistema-de-login/cadastro/cadastro.page').then( m => m.CadastroPage)
+  },
+  {
+    path: 'esqueci-senha',
+    loadComponent: () => import('./sistema-de-login/esqueci-senha/esqueci-senha.page').then( m => m.EsqueciSenhaPage)
+  },
+  
 ];
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
-
+import { IonicModule } from '@ionic/angular';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
