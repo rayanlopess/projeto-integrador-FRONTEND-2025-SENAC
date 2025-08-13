f653b699b17e69b9089fbcf985ac6fcfc847a2b8
Your Name <you@example.com>
2025-08-13 01:13:46 -0300

    finalizado tela de login

--- src/app/sistema-de-login/login/login.page.ts
+++ src/app/sistema-de-login/login/login.page.ts
@@ -1,26 +1,59 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonicModule } from '@ionic/angular';
 import { AutenticacaoService } from 'src/app/service/autenticacao.service';
+
+import { addIcons } from 'ionicons';
+import { add, trash, chevronDown, personCircle, lockClosed, person, lockOpen } from 'ionicons/icons';
+import { Router, RouterLink } from '@angular/router';
+import { AlertController } from '@ionic/angular/standalone';
+
+import { Title } from '@angular/platform-browser';
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonicModule, CommonModule, FormsModule]
 })
 export class LoginPage implements OnInit {
 
+  isPasswordVisible = false;
+   isAnimating = false;
   public login:string = '';
   public senha:string = '';
 
   constructor(
-    public autenticacaoService: AutenticacaoService
-  ) { }
+    public titleService: Title,
+    public autenticacaoService: AutenticacaoService,
+    public alertController: AlertController,
+    public rt: Router
+  ) { 
+    addIcons({ add, trash, chevronDown, personCircle, lockClosed, person, lockOpen });
+  }
 
   ngOnInit() {
+
+    this.titleService.setTitle("Login");
+
   }
+
+
+
+  togglePasswordVisibility() {
+    this.isAnimating = true;
+    this.isPasswordVisible = !this.isPasswordVisible;
+    
+    setTimeout(() => {
+      this.isAnimating = false;
+    }, 300); // Tempo igual à duração da animação
+  }
+  esqueciSenha(){
+    this.rt.navigateByUrl('/esqueci-senha');
+  }
+
   logar(){
     let login = this.login;
     let senha = this.senha;
